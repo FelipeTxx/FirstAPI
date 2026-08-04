8967a333af4284b011eecb4615a01a6f6ab2f9c1
FelipeTx - Notebook <feliperemes1@gmail.com>
2026-08-04 14:22:01 -0300

    adicionada funcionalidade de alteranar entre cadastro ou login

--- FirstFrontend/src/App.jsx
+++ FirstFrontend/src/App.jsx
@@ -4,20 +4,44 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Login from './pages/login'
 import Cadastro from './pages/cadastro'
+import { useEffect } from 'react';
 
 import './App.css'
 
+
 function App() {
- 
+  const [paginaAtual, setPaginaAtual] = useState("Login")
+  const [textoParaMudarPagina, setTextoParaMudarPagina] = useState("click")
+
+  function mudarLoginCadastro() {
+    if (paginaAtual === "Login") {
+      setPaginaAtual("Cadastro")
+      setTextoParaMudarPagina("Não tem conta? clique aqui!")
+    }
+    else{
+      setPaginaAtual("Login")
+      setTextoParaMudarPagina("Já uma tem conta? clique aqui!")
+    }
+  }
+  function setarTexto(){
+    paginaAtual === "Login" ? setTextoParaMudarPagina("Não tem conta? clique aqui!") : setTextoParaMudarPagina("Já tem uma conta? clique aqui!")
+  }
+  useEffect(() => {
+    setarTexto();
+  }, []);
+
+
   return (
-    
+
 
 
     <div>
-      <Cadastro/> 
+      <span></span>
+      <span on={()=>setarTexto()}>{paginaAtual === "Login" ? <Login/> : <Cadastro/>}</span>
+      <span className='irLogin' onClick={() => mudarLoginCadastro()}>{textoParaMudarPagina}</span>
     </div>
 
-    
+
   )
 
 }
